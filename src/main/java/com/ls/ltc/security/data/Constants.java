bcb60243e18b823a6cf23d5fa117a7fd10317479
Yolen Wang <yolen@leapstack.cn>
2017-07-20 15:35:02 +0800

    added ping message in controller

---
+++ src/main/java/com/ls/ltc/security/data/Constants.java
@@ -0,0 +1,7 @@
+package com.ls.ltc.security.data;
+
+public interface Constants {
+	public static String LTC_CURRENT_USER_ID = "ltc.principal";
+	public static String LTC_CURRENT_USER_ROLE = "ltc.role";
+	public static String LTC_CURRENT_REQUEST_TS = "ltc.request.ts";
+}
